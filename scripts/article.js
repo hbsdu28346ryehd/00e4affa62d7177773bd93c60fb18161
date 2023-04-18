b87dbb68697cb5a6cbe0f384024eefeb39a4b49e
Ritik-Swami <ritikswami50@gmail.com>
2023-04-18 18:51:32 +0530

    share btn update for mobile

--- scripts/article.js
+++ scripts/article.js
@@ -9,7 +9,6 @@ window.addEventListener('scroll', () => {
 	}
 });
 
-
 function copyToClipboard(text) {
 	var temp = document.createElement("input");
 	document.body.appendChild(temp);
@@ -19,3 +18,24 @@ function copyToClipboard(text) {
 	document.body.removeChild(temp);
 	alert("Link copied to clipboard!");
 }
+
+const shareBtn = document.getElementById('shareBtn')
+
+shareBtn.addEventListener('click', event => {
+	// Check for Web Share api support
+	if (navigator.share) {
+		const shareText = document.querySelector("input[name='article-share-text']").value;
+		const shareUrl = document.querySelector("input[name='article-share-url']").value;
+		// Browser supports native share api
+		navigator.share({
+			text: shareText,
+			url: shareUrl
+		}).then(() => {
+			console.log('Thanks for sharing!');
+		})
+			.catch((err) => console.error(err));
+	} else {
+		// Fallback
+		alert("The current browser does not support the share function. Please, manually share the link")
+	}
+});
